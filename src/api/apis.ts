6dee8760af91c780b954a32867290e987e51451a
DivineDemon <supame123@gmail.com>
2025-09-04 05:03:29 +0500

    Bug Fix.

--- src/api/apis.ts
+++ src/api/apis.ts
@@ -1,5 +1,5 @@
 export const postToWorkflow = async (username: string, repo: string, content: string) => {
-  const response = await fetch("https://n8n-hdw9.onrender.com/webhook-test/52174b3c-7cb9-4ead-ae45-f296af5d188e", {
+  const response = await fetch("https://n8n-hdw9.onrender.com/webhook/52174b3c-7cb9-4ead-ae45-f296af5d188e", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
